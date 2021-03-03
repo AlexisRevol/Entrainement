c08d6a486331aae329227f1548b442382903194a
Revol Alexis <revol.alexis.lgm@gmail.com>
2021-03-03 10:44:01 +0100

    Fichier sans conflit

--- fichier3.py
+++ fichier3.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 print("L'accroche fait mouche !!!")
-=======
+
 blablablabalbal
->>>>>>> 63c498a6be4daa4ce9399a4ca5fb66b22af74b31
+
 
 print("Nathan provoque une bonne poilade à Jordan")
